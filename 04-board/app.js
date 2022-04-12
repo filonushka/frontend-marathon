98f7a9c0a52961d6ae1fca4f5270abd9f0137c63
Valeriya Filonova <70750996+filonushka@users.noreply.github.com>
2022-04-13 01:49:06 +0300

    feat: add board project

---
+++ 04-board/app.js
@@ -0,0 +1,31 @@
+const board=document.querySelector('#board')
+const colors= ['#5F3CA4', '#C7B6E9', '#F5E2B9', '#A623A1', '#B1D098', '#99AA8C', '#FFF075', '#E9CEE0']
+const SQUARES_NUMBER = 500
+
+for (let i=0; i<SQUARES_NUMBER; i++) {
+    const square =document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+
+    square.addEventListener('mouseleave', removeColor)
+
+    board.append(square)
+}
+
+function setColor(event) {
+    const element = event.target
+    const color = getRandomColor()
+    element.style.backgroundColor= color 
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(event) {    
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor () {
+   return colors[Math.floor(Math.random()*colors.length)]
+}
